014930af6977bc4af23f64b2f09d5beb5a4899e8
SajjadAlizadehMoghadam <sajjadalizadeh6025@gmail.com>
2024-01-25 15:50:33 +0330

    Add project files.

---
+++ Show_Hide a Control/AppViewModdel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Show_Hide_a_Control
+{
+    internal class AppViewModdel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChange(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+        bool _ispanlShow;
+
+        public AppViewModdel()
+        {
+            _ispanlShow = false;
+        }
+
+        public bool IspanlShow
+        {
+            get { return _ispanlShow; }
+            set
+            {
+                _ispanlShow = value;
+                OnPropertyChange("IspanlShow");
+            }
+        }
+
+    }
+}
---
+++ Show_Hide a Control/ReplyCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Show_Hide_a_Control
+{
+    internal class ReplyCommand : ICommand
+    {
+        readonly Action<object> _action;
+        readonly Predicate<object> _predicate;
+
+
+        public ReplyCommand(Action<object> action)
+        {
+            _action = action;
+            _predicate = null;
+        }
+
+        public event EventHandler CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _predicate == null || CanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
